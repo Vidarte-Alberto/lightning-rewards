e3107ad369d96642cdd0b5d808eba9da933f22ef
Ivan Robles <irae45@gmail.com>
2026-08-20 20:39:37 -0600

    feat(landing): wire up routing.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,15 @@
-import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-neutral-800">
-      <img src="https://raw.githubusercontent.com/Sharmaz/enchilada/refs/heads/main/media/enchilada-js-logo.svg" width="250" alt="enchiladajs logo"/>
-      <div className="p-8">
-        <button
-          className="bg-neutral-900 px-4 py-2 rounded-lg text-slate-200 w-[160px] border-1 border-transparent hover:border-[#FF9811] transition-colors duration-250 cursor-pointer"
-          onClick={() => setCount((counter) => counter + 1)}
-          type="button"
-        >
-          {`Enchiladas ${count}`}
-        </button>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }
 
